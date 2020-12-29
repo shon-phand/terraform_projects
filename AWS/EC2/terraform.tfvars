3f1af7f2ab6f968d15052be66992a78f3ae188e7
shon <shonphand@gmail.com>
2020-12-29 13:11:29 +0000

    removed sensitive info

--- AWS/EC2/terraform.tfvars
+++ AWS/EC2/terraform.tfvars
@@ -1,4 +1,1 @@
-#aws_access_key = "AKIA4SGDDWLI5FX6ZYOB" 
-#aws_secret_access_key = "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"
 key_name = "web-kp"
-#aws_private_key_path = "./web-kp.pem"
---
+++ terraform_backend.tf
@@ -0,0 +1,12 @@
+#Set S3 backend for persisting TF state file remotely, ensure bucket already exits
+# And that AWS user being used by TF has read/write perms
+terraform {
+  required_version = ">=0.12.0"
+
+  backend "s3" {
+    region  = "us-east-1"
+    profile = "default"
+    key     = "terraformstatefile"
+    bucket  = "shonphand-tf-statefile"
+  }
+}
